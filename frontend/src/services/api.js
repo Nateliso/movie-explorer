42721302e47c384301b0d2f8ef9b758842c2fbf8
Liso Mhlana <mhlanaliso18@gmail.com>
2025-10-05 11:52:02 +0200

    Updated API URLs to use Render backend

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = import.meta.env.VITE_API_URL; // Backend URL
+const API_URL = "https://movie-explorer-6q65.onrender.com"; // Backend URL
 
 axios.defaults.withCredentials = true;
 
